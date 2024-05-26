cb2c15ee5fe02062fef4d9e5c96c4e9e02fc4e47
Jose-Prince <joseprince148@gmail.com>
2024-05-26 16:29:59 -0600

    Feat: Sidebar Election

--- src/components/Sidebar/ButtonsDisplay.tsx
+++ src/components/Sidebar/ButtonsDisplay.tsx
@@ -1,11 +1,13 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 import styles from './Sidebar.module.css'
 import { NavigationButton } from "./NavigationButton";
+import { useLocation } from "@builder.io/qwik-city";
 
 export const ButtonsDisplay = component$(()=>{
+
     return (
         <div class={styles.sidebar}>
-            <NavigationButton label="About me"/>
+            <NavigationButton label={'About me'}/>
             <NavigationButton label="My projects"/>
             <NavigationButton label="Technologies"/>
         </div>
--- src/components/Sidebar/NavigationButton.tsx
+++ src/components/Sidebar/NavigationButton.tsx
@@ -1,16 +1,36 @@
-import { component$ } from "@builder.io/qwik";
-import styles from './Sidebar.module.css'
-import { useNavigate } from "@builder.io/qwik-city";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+import styles from './Sidebar.module.css';
+import { useLocation, useNavigate } from "@builder.io/qwik-city";
 
 interface ItemsProps {
-    label : string
+    label: string;
 }
 
-export const NavigationButton = component$<ItemsProps>((props)=>{
-    const navigate = useNavigate()
+export const NavigationButton = component$<ItemsProps>((props) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const state = useSignal(false);
+
+    useTask$(({ track }) => {
+        track(() => location.url.pathname)
+        const encodedLocation = location.url.pathname
+        const decodedLocation = decodeURIComponent(encodedLocation).replace(/\/$/, '').replace('/', '')      
+        
+        if (decodedLocation === props.label) {
+            state.value = true;
+        } else {
+            state.value = false;
+        }
+    });
 
     return (
-        <button class={styles.navButton}
-            onClick$={() => navigate('/'+props.label)}><p class={styles.textRot}>{props.label.toUpperCase()}</p></button>
-    )
-})
+        <button
+            class={state.value ? styles.navButtonA : styles.navButton}
+            onClick$={() => navigate('/' + props.label)}
+        >
+            <p class={styles.textRot}>
+                {props.label.toUpperCase()}
+            </p>
+        </button>
+    );
+});

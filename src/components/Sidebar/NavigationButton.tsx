5ce4319ff7b8cda56d6258a923cd4546165ba499
Jose-Prince <joseprince148@gmail.com>
2024-05-26 12:34:29 -0600

    Fix: Text button navigation displayment

--- src/components/Sidebar/NavigationButton.tsx
+++ src/components/Sidebar/NavigationButton.tsx
@@ -1,12 +1,16 @@
 import { component$ } from "@builder.io/qwik";
 import styles from './Sidebar.module.css'
+import { useNavigate } from "@builder.io/qwik-city";
 
 interface ItemsProps {
     label : string
 }
 
 export const NavigationButton = component$<ItemsProps>((props)=>{
+    const navigate = useNavigate()
+
     return (
-        <button class={styles.navButton}>{props.label}</button>
+        <button class={styles.navButton}
+            onClick$={() => navigate('/'+props.label)}><p class={styles.textRot}>{props.label.toUpperCase()}</p></button>
     )
 })
--- src/routes/Technologies/index.tsx
+++ src/routes/Technologies/index.tsx
@@ -1,7 +1,8 @@
 import { component$ } from "@builder.io/qwik";
+import { ButtonsDisplay } from "~/components/Sidebar/ButtonsDisplay";
 
 export default component$(() => {
     return (
-        4
+        <ButtonsDisplay/>
     )
 })

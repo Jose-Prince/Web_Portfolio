62357840b0232ce4691e274c0b3e1364749110dd
Jose-Prince <joseprince148@gmail.com>
2024-05-26 22:32:43 -0600

    Feat: My projects display skeleton

---
+++ src/components/About/Reference.tsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+import { Imagen } from "./Imagen";
+
+interface ItemsProps {
+    link: string
+    ref: string
+    ancho: string
+}
+
+export const Reference = component$<ItemsProps>((props)=>{
+    return <a href={props.link} style={{padding: '15px'}}>
+        <Imagen ref={props.ref} ancho={props.ancho}/>
+    </a>
+})

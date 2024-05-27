b8199e8fd98ecec96654c7af2a71e1692df46b51
Jose-Prince <joseprince148@gmail.com>
2024-05-26 19:34:37 -0600

    Feat: name and description

--- src/routes/About me/index.tsx
+++ src/routes/About me/index.tsx
@@ -1,8 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { ButtonsDisplay } from "~/components/Sidebar/ButtonsDisplay";
 import styles from '../../components/About/About.module.css'
-import image from '../../../public/PrinceTransparent.png'
-import { Imagen } from "~/components/About/Imagen";
 import { Circle } from "~/components/About/Circle";
 
 export default component$(() => {
@@ -11,6 +9,8 @@ export default component$(() => {
             <ButtonsDisplay/>
             <div class={styles.aboutDisplay}>
                 <Circle />
+                <h1>José Alejandro Prince Martinez</h1>
+                <div class={styles.description}>Programador desde hace 2 años. Actualmente estudiante en la Universidad del Valle de Guatemala en la carrera de Licenciatura en Ingenieria en Ciencias de de la computación y Tecnologías de la Información. Sin experencia laboral como programador junior. Conocimiento variado en diferentes lenguajes tanto de Back-end y Front-end. Se tiene un gusto principalmente en el desarrollo de Front-end tanto para aplicaciones móviles como para páginas web, dominando principalmente las tecnologías web. Se tiene un gusto por el aprendizaje, lo que hace que pruebe diferentes tecnologías para tener una mejor noción sobre el uso de las diferentes tecnologías que existen y cuando es mejor aplicar cada una de ellas. </div>
             </div>
         </>
     )

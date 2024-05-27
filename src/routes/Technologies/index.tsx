35dd0a7764b4d84dd38a93440d477ba9247b4672
Jose-Prince <joseprince148@gmail.com>
2024-05-27 10:39:24 -0600

    Feat: grid display skeleton

--- src/routes/Technologies/index.tsx
+++ src/routes/Technologies/index.tsx
@@ -1,8 +1,19 @@
 import { component$ } from "@builder.io/qwik";
 import { ButtonsDisplay } from "~/components/Sidebar/ButtonsDisplay";
+import styles from './Tech.module.css'
 
 export default component$(() => {
     return (
-        <ButtonsDisplay/>
+        <>
+            <ButtonsDisplay/>
+            <div class={styles.techDisplay}>
+                <p>ddsadas</p>
+                <p>ddsadas</p>
+                <p>ddsadas</p>
+                <p>ddsadas</p>
+                <p>ddsadas</p>
+                <p>ddsadas</p>
+            </div>
+        </>
     )
 })

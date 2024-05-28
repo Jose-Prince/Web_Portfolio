b8837e8e3132b7e44196bf15a09a09c85aa41fab
Jose-Prince <joseprince148@gmail.com>
2024-05-28 01:38:15 -0600

    Fix: Technologies images

--- src/components/Technologie/techItem.tsx
+++ src/components/Technologie/techItem.tsx
@@ -4,12 +4,24 @@ import styles from './Technology.module.css';
 interface ItemsProps {
     link: string;
     name: string;
+    width: string;  // Nueva propiedad para el ancho
+    aspectRatio: number; // Nueva propiedad para la relación de aspecto
 }
 
-export const TechItem = component$<ItemsProps>(({ link, name }) => {
+export const TechItem = component$<ItemsProps>(({ link, name, width, aspectRatio }) => {
+    const widthNum = parseFloat(width);
+    const heightNum = widthNum / aspectRatio;
+
     return (
         <div class={styles.techItem}>
-            <img src={link} alt={name} width="10" height="20"/>
+            <img
+                src={link}
+                alt={name}
+                class={styles.techImage}
+                style={{ width: `${widthNum}vw`, height: `${heightNum}vw` }}
+                width={widthNum}
+                height={heightNum}
+            />
             <h2 class={styles.techName}>{name}</h2>
         </div>
     );

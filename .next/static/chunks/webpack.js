ccb49cf4770dcef8a16e0b57c4260b72e06d51b0
Jose-Prince <joseprince148@gmail.com>
2024-05-30 14:01:13 -0600

    Fix: Text Label Technology color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc86b685f0f5adf8"; }
+/******/ 		__webpack_require__.h = function() { return "6a5a46662bce2a1e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
